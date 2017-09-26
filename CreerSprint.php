6ad02e7164abe1a9ca9923349c6d00c48dff0fd1
Jeremy <j.benaim@orange.fr>
2017-09-27 01:46:35 +0200

    Optimisation et création
    Meilleur indentation du code en général
    Création d'un index pour le moment vide comme premiere page au lieu du dashboard

--- CreerSprint.php
+++ CreerSprint.php
@@ -49,87 +49,86 @@
 					</div>
 				</div>
 			</div>
-    <!-- Bootstrap core JavaScript-->
-    <?php require_once __Dir__ . '/footer.php'; ?>
-    </div>
-    </body>
-		<script>
-			//Creation du format des datatimepicker avec un format ok pour l'insertion dans la bdd, un close auto lorsque l'on choisie la date et un view a 2 car on a pas besoin de plus.
-			$('#dateDebut').datetimepicker({
-			format: 'yyyy-mm-dd',
-			autoclose: true,
-			minView : 2
-			});
-			$('#dateFin').datetimepicker({
-			format: 'yyyy-mm-dd',
-			autoclose: true,
-			minView : 2
-			});
-			
-			//Fonction pour auto remplir la date d'aujourd'hui dans le premier input date
-			function DateAujourdhui(_id){
-			var _dat = document.querySelector(_id);
-			var aujourdui = new Date(),
-			j = aujourdui.getDate(),
-			m = aujourdui.getMonth()+1, 
-			a = aujourdui.getFullYear(),
-			data;
-			
-			//si jour ou mois inferrieur a 10 genre "1" il doit avoir un "0" avant pour que le date soit dans un format valide.   
-			if(j < 10){
-			j = "0" + j;
-			};
-			if(m < 10){
-			m = "0" + m;
-			};
-			data = a + "-" + m + "-" + j;
-			_dat.value = data;
-			};
-			DateAujourdhui("#dateDebut");
-			
-			//Mettre le deuxieme datapicker à 14jours après la date d'aujourd'hui.
-			function DateApres(_id){
-			var _dat = document.querySelector(_id);
-			var Apres = new Date(),
-			j = Apres.getDate()+14,
-			m = Apres.getMonth()+1, 
-			a = Apres.getFullYear(),
-			data;
-			
-			//Si l'on dépasse 31jours avec le bon mois, faire le calcule.
-			if(m == (1||3||5||7||8||10||12)) {  
-			if(j > 31){
-			j -= 31;
-			m += 1;     
-			}
-			}
-			else{ 
-			if(j > 30){
-			j -= 30; 
-			m += 1;      
-			};
-			}; 
-			
-			//si mois dépasse 12 alors passer à l'année prochaine et remettre le bon mois.
-			if(m > 12){
-			m -= 12;
-			y += 1;
-			};
-			
-			if(j < 10){
-			j = "0"+j;
-			};
-			
-			if(m < 10){
-			m = "0"+m;
-			};
-			
-			data = a + "-" + m + "-" + j;
-			_dat.value = data;
-			};
-			
-			DateApres("#DateFin");
-			
-		</script>
-	
+			<!-- Bootstrap core JavaScript-->
+			<?php require_once __Dir__ . '/footer.php'; ?>
+		</div>
+	</body>
+	<script>
+		//Creation du format des datatimepicker avec un format ok pour l'insertion dans la bdd, un close auto lorsque l'on choisie la date et un view a 2 car on a pas besoin de plus.
+		$('#dateDebut').datetimepicker({
+		format: 'yyyy-mm-dd',
+		autoclose: true,
+		minView : 2
+		});
+		$('#dateFin').datetimepicker({
+		format: 'yyyy-mm-dd',
+		autoclose: true,
+		minView : 2
+		});
+		
+		//Fonction pour auto remplir la date d'aujourd'hui dans le premier input date
+		function DateAujourdhui(_id){
+		var _dat = document.querySelector(_id);
+		var aujourdui = new Date(),
+		j = aujourdui.getDate(),
+		m = aujourdui.getMonth()+1, 
+		a = aujourdui.getFullYear(),
+		data;
+		
+		//si jour ou mois inferrieur a 10 genre "1" il doit avoir un "0" avant pour que le date soit dans un format valide.   
+		if(j < 10){
+		j = "0" + j;
+		};
+		if(m < 10){
+		m = "0" + m;
+		};
+		data = a + "-" + m + "-" + j;
+		_dat.value = data;
+		};
+		DateAujourdhui("#dateDebut");
+		
+		//Mettre le deuxieme datapicker à 14jours après la date d'aujourd'hui.
+		function DateApres(_id){
+		var _dat = document.querySelector(_id);
+		var Apres = new Date(),
+		j = Apres.getDate()+14,
+		m = Apres.getMonth()+1, 
+		a = Apres.getFullYear(),
+		data;
+		
+		//Si l'on dépasse 31jours avec le bon mois, faire le calcule.
+		if(m == (1||3||5||7||8||10||12)) {  
+		if(j > 31){
+		j -= 31;
+		m += 1;     
+		}
+		}
+		else{ 
+		if(j > 30){
+		j -= 30; 
+		m += 1;      
+		};
+		}; 
+		
+		//si mois dépasse 12 alors passer à l'année prochaine et remettre le bon mois.
+		if(m > 12){
+		m -= 12;
+		y += 1;
+		};
+		
+		if(j < 10){
+		j = "0"+j;
+		};
+		
+		if(m < 10){
+		m = "0"+m;
+		};
+		
+		data = a + "-" + m + "-" + j;
+		_dat.value = data;
+		};
+		
+		DateApres("#DateFin");
+		
+	</script>
 </html>
--- footer.php
+++ footer.php
@@ -1,36 +1,40 @@
-		    <footer class="sticky-footer">
-      <div class="container">
-        <div class="text-center">
-          <small>Copyright © Your Website 2017</small>
+    <footer class="sticky-footer">
+        <div class="container">
+            <div class="text-center">
+                Copyright © Ns Scrum 2017 <i class="fa fa-github" aria-hidden="true"></i><a href="https://github.com/Jeje2201/LeNouveauScrum">  Projet Github</a> <i class="fa fa-globe" aria-hidden="true"></i><a href="http://mrjeje.esy.es/">  Website</a>
+            </div>
         </div>
-      </div>
     </footer>
+
     <!-- Scroll to Top Button-->
     <a class="scroll-to-top rounded" href="#page-top">
-      <i class="fa fa-angle-up"></i>
+    <i class="fa fa-angle-up"></i>
     </a>
+
     <!-- Logout Modal-->
     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">×</span>
-            </button>
-          </div>
-          <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-          <div class="modal-footer">
-            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-            <a class="btn btn-primary" href="login.html">Logout</a>
-          </div>
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="login.html">Logout</a>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>   
+    </div>
 
-   <!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
+    <!-- Classe pour le Hightsharr-->
     <script src="js/highcharts.js"></script>
+    <!-- Bootstrap core JavaScript-->
     <script src="vendor/popper/popper.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
     <!-- Core plugin JavaScript-->
@@ -44,17 +48,13 @@
     <!-- Custom scripts for this page-->
     <script src="js/sb-admin-datatables.min.js"></script>
     <script src="js/sb-admin-charts.min.js"></script>
-
     <!-- BootStrap pour les heures-->
     <script src="js/bootstrap-datetimepicker.js"></script>
-    <!-- Classe pour le Hightsharr-->
-            
 
-    
     <script>
-     $('#toggleNavColor').click(function() {
-      $('nav').toggleClass('navbar-dark navbar-light');
-      $('nav').toggleClass('bg-dark bg-light');
-      $('body').toggleClass('bg-dark bg-light');
-    });
+        $('#toggleNavColor').click(function() {
+         $('nav').toggleClass('navbar-dark navbar-light');
+         $('nav').toggleClass('bg-dark bg-light');
+         $('body').toggleClass('bg-dark bg-light');
+        });
     </script>
