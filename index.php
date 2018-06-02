17fed82bf6e34c87380318644d98a0d496edbc7b
MrJeje_ <jeremy.leriche22@gmail.com>
2018-06-02 19:41:46 +0200

    Ajout de la config de VsCode

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 						<h1>Scrum</h1>
 						<p><b>Scrum</b> est un schéma d'organisation de développement de produits complexes. Il est défini par ses créateurs comme un « cadre de travail holistique itératif qui se concentre sur les buts communs en livrant de manière productive et créative des produits de la plus grande valeur possible ».</p>
 						<img src="https://media.giphy.com/media/xT1XGOGdyDrL2BTfxK/giphy.gif" alt="HTML5 Icon">
-						</br>
+						</br> 
 						</br>
 						<i class="fa fa-github" aria-hidden="true"></i><a href="https://github.com/Jeje2201/LeNouveauScrum">  Projet Github</a> <i class="fa fa-globe" aria-hidden="true"></i><a href="http://mrjeje.esy.es/">  Website</a>
 					</div>
