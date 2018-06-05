3711c1c40fb24784ea76b5fd5c142a844ba8c2fc
MrJeje_ <jeremy.leriche22@gmail.com>
2018-06-05 10:59:04 +0200

    DateTime connait tout seul la date dans 14 jours
    + La précédente fonction checké en fonction du mois si il y avait 30 ou 31 jours, les mois étant écris en dur dans une liste js.
    Maintenant la fonction connait par elle meme quelle jours sera celui dans 14, sans indiqué les mois en array.
    + La fonction pour mettre un 0 dans les Jours et Mois si la valeur est < 10 est maintenant a part, au lieu d'etre collé a plusieurs endroit, et est appelé quand besoin.

--- CreerSprint.php
+++ CreerSprint.php
@@ -1,132 +1,106 @@
 <!DOCTYPE html>
 <html lang="en">
-	<!-- Obtenir le nombre de sprint -->
-	<?php include('header.php'); ?>
-	<?php $req = $conn->query('SELECT numero as nummax from sprint where id = (SELECT max(id) FROM sprint)');
-		$data = $req->fetch_assoc();
-		?>
-	<body class="fixed-nav sticky-footer" id="page-top">
-		<div class="content-wrapper">
-			<div class="container">
-				<div class="card card-register mx-auto mt-5">
-					<div class="card-header">Créer un Sprint</div>
-					<div class="card-body">
-						<form method="POST" role="form" action="EditerBdd\AjoutSprint.php">
-                            <!-- Numero du nouveau sprint -->
-							<div class="form-group">
-								<label for="exampleInputEmail1">Numéro Sprint</label>
-								<input class="form-control" id="numero" name="numero" type="number" aria-describedby="emailHelp" placeholder="Le texte" min="<?php echo $data['nummax']+1; ?>" max="<?php echo $data['nummax']+1; ?>" value="<?php echo $data['nummax']+1; ?>">
-							</div>
-							<div class="form-group">
-								<div class="form-row">
-                                    <!-- Heure de début -->
-									<div class="col-md-6">
-										<label for="exampleInputName">Heure de Début</label>
-										<div class='input-group date'>
-											<input type='text' placeholder="Date de Début"  name="dateDebut" id='dateDebut' class="form-control" />
-											<span class="input-group-addon">
+<!-- Obtenir le nombre de sprint -->
+<?php include('header.php'); ?>
+<?php $req = $conn->query('SELECT numero as nummax from sprint where id = (SELECT max(id) FROM sprint)');
+$data = $req->fetch_assoc();
+?>
+<body class="fixed-nav sticky-footer" id="page-top">
+	<div class="content-wrapper">
+		<div class="container">
+			<div class="card card-register mx-auto mt-5">
+				<div class="card-header">Créer un Sprint</div>
+				<div class="card-body">
+					<form method="POST" role="form" action="EditerBdd\AjoutSprint.php">
+						<!-- Numero du nouveau sprint -->
+						<div class="form-group">
+							<label for="exampleInputEmail1">Numéro Sprint</label>
+							<input class="form-control" id="numero" name="numero" type="number" aria-describedby="emailHelp" placeholder="Le texte" min="<?php echo $data['nummax']+1; ?>" max="<?php echo $data['nummax']+1; ?>" value="<?php echo $data['nummax']+1; ?>">
+						</div>
+						<div class="form-group">
+							<div class="form-row">
+								<!-- Heure de début -->
+								<div class="col-md-6">
+									<label for="exampleInputName">Heure de Début</label>
+									<div class='input-group date'>
+										<input type='text' placeholder="Date de Début"  name="dateDebut" id='dateDebut' class="form-control" />
+										<span class="input-group-addon">
 											<span class="fa fa-calendar"></span>
-											</span>
-										</div>
+										</span>
 									</div>
-                                    <!-- Heure de fin -->
-									<div class="col-md-6">
-										<label for="exampleInputLastName">Heure de Fin</label>
-										<div class='input-group date'>
-											<input type='text' placeholder="Date de Début"  name="dateFin" id='dateFin' class="form-control" />
-											<span class="input-group-addon">
+								</div>
+								<!-- Heure de fin -->
+								<div class="col-md-6">
+									<label for="exampleInputLastName">Heure de Fin</label>
+									<div class='input-group date'>
+										<input type='text' placeholder="Date de Début"  name="dateFin" id='dateFin' class="form-control" />
+										<span class="input-group-addon">
 											<span class="fa fa-calendar"></span>
-											</span>
-										</div>
+										</span>
 									</div>
 								</div>
 							</div>
-							<button type="submit" class="btn btn-primary btn-block">
+						</div>
+						<button type="submit" class="btn btn-primary btn-block">
 							<span class="glyphicon glyphicon-upload" aria-hidden="true"></span> Créer
-							</button>
-						</form>
-					</div>
+						</button>
+					</form>
 				</div>
 			</div>
 		</div>
-        <?php require_once __Dir__ . '/footer.php'; ?>
+	</div>
+	<?php require_once __Dir__ . '/footer.php'; ?>
 	<script>
+
 		//Creation du format des datatimepicker avec un format ok pour l'insertion dans la bdd, un close auto lorsque l'on choisie la date et un view a 2 car on a pas besoin de plus.
 		$('#dateDebut').datetimepicker({
-		format: 'yyyy-mm-dd',
-		autoclose: true,
-		minView : 2
+			format: 'yyyy-mm-dd',
+			autoclose: true,
+			minView : 2
 		});
 		$('#dateFin').datetimepicker({
-		format: 'yyyy-mm-dd',
-		autoclose: true,
-		minView : 2
+			format: 'yyyy-mm-dd',
+			autoclose: true,
+			minView : 2
 		});
-		
+
+		DateAujourdhui("#dateDebut");
+		DateApres("#dateFin");
+
 		//Fonction pour auto remplir la date d'aujourd'hui dans le premier input date
 		function DateAujourdhui(_id){
-		var _dat = document.querySelector(_id);
-		var aujourdui = new Date(),
-		j = aujourdui.getDate(),
-		m = aujourdui.getMonth()+1, 
-		a = aujourdui.getFullYear(),
-		data;
-		
-		//si jour ou mois inferrieur a 10 genre "1" il doit avoir un "0" avant pour que le date soit dans un format valide.   
-		if(j < 10){
-		j = "0" + j;
+			var _dat = document.querySelector(_id);
+			var aujourdui = new Date(),
+			j = aujourdui.getDate(),
+			m = aujourdui.getMonth()+1, 
+			a = aujourdui.getFullYear();
+
+			Ajouter0JoursMois(j,m,a,_dat)
 		};
-		if(m < 10){
-		m = "0" + m;
-		};
-		data = a + "-" + m + "-" + j;
-		_dat.value = data;
-		};
-		DateAujourdhui("#dateDebut");
-		
+
 		//Mettre le deuxieme datapicker à 14jours après la date d'aujourd'hui.
 		function DateApres(_id){
-		var _dat = document.querySelector(_id);
-		var Apres = new Date(),
-		j = Apres.getDate()+14,
-		m = Apres.getMonth()+1, 
-		a = Apres.getFullYear(),
-		data;
+			var _dat = document.querySelector(_id);
+			var Apres = new Date();
+			Apres.setDate(Apres.getDate()+14);
+			j = Apres.getDate(),
+			m = Apres.getMonth()+1, 
+			a = Apres.getFullYear();
+
+			Ajouter0JoursMois(j,m,a,_dat)
+		};
 		
-		//Si l'on dépasse 31jours avec le bon mois, faire le calcule.
-		if(m == (1||3||5||7||8||10||12)) {  
-		if(j > 31){
-		j -= 31;
-		m += 1;     
+		function Ajouter0JoursMois(j,m,a,_dat){
+			if(j < 10){
+				j = "0" + j;
+			};
+			if(m < 10){
+				m = "0" + m;
+			};
+
+			_dat.value = a + "-" + m + "-" + j;
 		}
-		}
-		else{ 
-		if(j > 30){
-		j -= 30; 
-		m += 1;      
-		};
-		}; 
-		
-		//si mois dépasse 12 alors passer à l'année prochaine et remettre le bon mois.
-		if(m > 12){
-		m -= 12;
-		y += 1;
-		};
-		
-		if(j < 10){
-		j = "0"+j;
-		};
-		
-		if(m < 10){
-		m = "0"+m;
-		};
-		
-		data = a + "-" + m + "-" + j;
-		_dat.value = data;
-		};
-		
-		DateApres("#dateFin");
-		
+
 	</script>
-    </body>
+</body>
 </html>
