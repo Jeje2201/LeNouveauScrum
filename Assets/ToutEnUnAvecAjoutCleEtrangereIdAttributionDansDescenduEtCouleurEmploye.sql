98bd4114bfa156ab84446c8d7ee0de3d564dff43
MrJeje_ <jeremy.leriche22@gmail.com>
2018-06-15 13:02:38 +0200

    Bdd ne se créé pas auto, fixé

--- Assets/ToutEnUnAvecAjoutCleEtrangereIdAttributionDansDescenduEtCouleurEmploye.sql
+++ Assets/ToutEnUnAvecAjoutCleEtrangereIdAttributionDansDescenduEtCouleurEmploye.sql
@@ -1,15 +1,18 @@
+
+CREATE DATABASE scrum;
+USE scrum;
+
+
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.6.4
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 14 juin 2018 à 11:37
--- Version du serveur :  5.7.19
--- Version de PHP :  5.6.31
+-- Client :  127.0.0.1
+-- Généré le :  Lun 13 Février 2017 à 10:10
+-- Version du serveur :  5.7.14
+-- Version de PHP :  5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
