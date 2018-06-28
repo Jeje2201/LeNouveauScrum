ccd94a41d6cef0a3a605a98fd6e909fb528ac1ef
MrJeje_ <jeremy.leriche22@gmail.com>
2018-06-28 21:53:18 +0200

    Script de barre de recherche n'enleve plus le thead :not(thead tr)

--- Assets/Js/BarreDeRecherche.js
+++ Assets/Js/BarreDeRecherche.js
@@ -1,11 +1,10 @@
    
 function BarreDeRecherche(IdDivBarreDeRecherche, IdTableRecherche){
 
-    $("#"+IdDivBarreDeRecherche+"").on("keyup", function() {
-      var value = $(this).val().toLowerCase();
-      $("#"+IdTableRecherche+" tr").filter(function() {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-      });
+  $("#"+IdDivBarreDeRecherche).on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#"+IdTableRecherche+" tr:not(thead tr)").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
-
+  });
 }
