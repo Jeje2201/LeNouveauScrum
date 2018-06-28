abe256dfefb451fdc063228d1e13d7e800a1fb35
MrJeje_ <jeremy.leriche22@gmail.com>
2018-06-28 17:04:20 +0200

    Généralisation de la méthode pour créer une barre de rechercher

---
+++ Assets/Js/BarreDeRecherche.js
@@ -0,0 +1,6 @@
+$("#BarreDeRecherche").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#myTable tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
