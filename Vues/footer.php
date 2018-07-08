a88b66fcba6e1a43cfbba6f911a12c2919fbdd70
MrJeje_ <jeremy.leriche22@gmail.com>
2018-07-08 22:44:16 +0200

    Suppression des crédits dans le footer, prend de la place pour rien

--- Vues/footer.php
+++ Vues/footer.php
@@ -1,12 +1,5 @@
 </body>
 <div id="snackbar">Some text some message..</div>
-<footer class="sticky-footer">
-	<div class="container">
-		<div class="text-center">
-			Copyright © Ns Scrum 2018 - <i class="fa fa-github" aria-hidden="true">  </i><a target="_blank" href="https://github.com/Jeje2201/ScrumManager"> Projet Github</a> - <i class="fa fa-globe" aria-hidden="true"></i><a target="_blank" href="http://mrjeje.esy.es/">  Jérémy</a>
-		</div>
-	</div>
-</footer>
 <!-- Scroll to Top Button-->
 <a class="scroll-to-top rounded" href="#page-top">
 	<i class="fa fa-angle-up"></i>
