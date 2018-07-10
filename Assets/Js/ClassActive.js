776a5b499818383b22ae7ce8ad67a2cdf5893308
MrJeje_ <jeremy.leriche22@gmail.com>
2018-07-10 17:34:20 +0200

    charger class activ apres que la navbar soit chargée

--- Assets/Js/ClassActive.js
+++ Assets/Js/ClassActive.js
@@ -1,2 +1,7 @@
 	var sPageURL = window.location.search.substring(1).split('=');
+
+	$( "li" ).each(function() {
+		$( this ).removeClass( "active" );
+	});
+	
 	$('nav a[href^="index.php?vue=' + sPageURL[1] + '"]').closest('li').addClass('active');
