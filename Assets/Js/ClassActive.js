db82626976bc6c889761fee4fa5423163bd2b501
MrJeje_ <jeremy.leriche22@gmail.com>
2018-07-30 15:40:34 +0200

    Stop avec l'affichage en haut a droite de la navbar

--- Assets/Js/ClassActive.js
+++ Assets/Js/ClassActive.js
@@ -4,15 +4,13 @@ function ClassActive(LaPage, TypeUser, NameUser){
 	$( "li" ).each(function() {
 		$( this ).removeClass( "active" );
 	});
-
-
-	$( "#TitreNavBar" ).html('<p style="position: absolute; top: 15px; color: white; right: 15px;"><i class="fa fa-user" aria-hidden="true"></i> '+NameUser+' - <a style="color: white"href="Modele/ConnectionLogout.php"><span class="nav-link-text">Déconnexion</span></a> <i class="fa fa-fw fa-sign-out"></i></p>');
-
-	//Chercher la nav qui a la page d'affiché et lui donner la class active
+	//Puis cherche la nav qui a la page d'affiché et lui donner la class active
 	$('nav a[href^="index.php?vue=' + LaPage + '"]').closest('li').addClass('active');
 
+
+	$( "#TitreNavBar" ).text('Ns Scrum - ' + NameUser);
+
 	//Cacher les onglets innutiles pour les non admin
-	console.log(TypeUser)
 	if(TypeUser !== 'ScrumMaster'){
 	$( ".AdminOnly" ).each(function() {
   		$( this ).hide();
