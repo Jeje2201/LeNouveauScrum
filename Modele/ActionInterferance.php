6247bf68f4a146f83dd69a9550c34d9a03505429
MrJeje_ <jeremy.leriche22@gmail.com>
2018-08-02 11:13:47 +0200

    Ajout d'un possible label pour les interferences

--- Modele/ActionInterferance.php
+++ Modele/ActionInterferance.php
@@ -8,7 +8,7 @@ require_once ('../Modele/Configs.php');
      if($_POST["action"] == "Load") 
      {
       $numero = $_POST["idAffiche"];
-      $statement = $connection->prepare("SELECT interference.id as id, interference.heure as Heure, (select Prenom from employe where employe.id = interference.id_Employe) as Employe, (select nom from projet where projet.id = interference.id_Projet) as Projet, (select nom from typeinterference where typeinterference.id = interference.id_TypeInterference) as Type FROM interference Where id_Sprint = $numero ORDER BY interference.id_Projet asc");
+      $statement = $connection->prepare("SELECT interference.id as id, interference.heure as Heure,interference.label as Label, (select Prenom from employe where employe.id = interference.id_Employe) as Employe, (select nom from projet where projet.id = interference.id_Projet) as Projet, (select nom from typeinterference where typeinterference.id = interference.id_TypeInterference) as Type FROM interference Where id_Sprint = $numero ORDER BY interference.id_Projet asc");
       $statement->execute();
       $result = $statement->fetchAll();
       $output = '';
@@ -16,10 +16,11 @@ require_once ('../Modele/Configs.php');
       <table class="table table-sm table-bordered" id="datatable" width="100%" cellspacing="0">
       <thead>
       <tr>
-      <th width="25%">Nombre d\'heures</th>
-      <th width="20%">Ressource</th>
+      <th width="5%">Heures</th>
+      <th width="15%">Ressource</th>
       <th width="20%">Projet</th>
-      <th width="25%">Type</th>
+      <th width="20%">Type</th>
+      <th width="20%">Label</th>
       <th width="10%"><center>Éditer</center></th>
       </tr>
       </thead>
@@ -35,6 +36,7 @@ require_once ('../Modele/Configs.php');
         <td>'.$row["Employe"].'</td>
         <td>'.$row["Projet"].'</td>
         <td>'.$row["Type"].'</td>
+        <td>'.$row["Label"].'</td>
         <td><center><div class="btn-group" role="group" aria-label="Basic example"><button type="button" id="'.$row["id"].'" class="btn btn-warning update"><i class="fa fa-pencil" aria-hidden="true"></i></button><button type="button" id="'.$row["id"].'" class="btn btn-danger delete"><i class="fa fa-times" aria-hidden="true"></i></button></div></center></td>
         </tr>
         ';
@@ -55,8 +57,8 @@ require_once ('../Modele/Configs.php');
  if($_POST["action"] == "Ajouter")
  {
   $statement = $connection->prepare("
-   INSERT INTO interference (heure, id_TypeInterference, id_Sprint, id_Projet, id_Employe) 
-   VALUES (:heure, :typeinterference, :sprint, :projet, :employe)
+   INSERT INTO interference (heure, id_TypeInterference, id_Sprint, id_Projet, id_Employe, label) 
+   VALUES (:heure, :typeinterference, :sprint, :projet, :employe, :label)
    ");
 
      $result = $statement->execute(
@@ -65,6 +67,7 @@ require_once ('../Modele/Configs.php');
     ':typeinterference' => $_POST["TypeInterferance"],
     ':sprint' => $_POST["idAffichee"],
     ':projet' => $_POST["Projet"],
+    ':label' => $_POST["labelinterference"],
     ':employe' => $_POST["Employe"]
   )
  );
@@ -91,6 +94,7 @@ if($_POST["action"] == "Select")
    $output["Sprint"] = $row["id_Sprint"];
    $output["Projet"] = $row["id_Projet"];
    $output["Employe"] = $row["id_Employe"];
+   $output["labelinterference"] = $row["label"];
  }
  echo json_encode($output);
 }
@@ -99,7 +103,7 @@ if($_POST["action"] == "Update")
 {
   $statement = $connection->prepare(
    "UPDATE interference 
-   SET heure = :heure, id_TypeInterference = :typeinterference, id_Sprint =:sprint, id_Projet = :projet, id_Employe = :employe
+   SET heure = :heure, id_TypeInterference = :typeinterference, id_Sprint =:sprint, id_Projet = :projet, id_Employe = :employe, label = :label
    WHERE id = :id
    "
  );
@@ -110,6 +114,7 @@ if($_POST["action"] == "Update")
     ':sprint' => $_POST["idAffichee"],
     ':projet' => $_POST["Projet"],
     ':employe' => $_POST["Employe"],
+    ':label' => $_POST["labelinterference"],
     ':id'   => $_POST["id"]
   )
  );
