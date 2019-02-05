6d29cfe37d6c86c7bbd5a3dba89d58adfe7b6e57
MrJeje_ <jeremy.leriche22@gmail.com>
2019-02-05 10:57:20 +0100

    suppression fichiers innutiles

--- Assets/Script/Backup.bat
+++
@@ -1,3 +0,0 @@
-c:
-cd "wamp64\bin\mysql\mysql5.7.21\bin\"
-mysqldump.exe -u root scrum > D:\Code\wwwamp\ScrumBackup%date:~0,2%-%date:~3,2%-%date:~6,4%@%time:~0,2%h%time:~3,2%.sql
--- Assets/Script/UpdateScrumApp.bat
+++
@@ -1,2 +0,0 @@
-rmdir /Q /S ScrumManager
-git clone https://www.github.com/Jeje2201/ScrumManager
