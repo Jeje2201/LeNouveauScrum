1a190a1c45c7bcdcf695c1e456b7a17bf68bb914
MrJeje_ <jeremy.leriche22@gmail.com>
2019-02-14 12:40:44 +0100

    separate default alert from error alert

--- Assets/Js/AlertBootStrap.js
+++ Assets/Js/AlertBootStrap.js
@@ -2,13 +2,26 @@
  * Permet de faire apparaitre une popup plus sympa
  * @param {string} message Le message a faire apparaitre dans la popup 
  */
-function BootstrapAlert(message) {
+function BootstrapAlertDefaut(message) {
 	$('#snackbar').addClass('show');
 
 	$('#snackbar').html(message);
 
 	setTimeout(function () {
 		$('#snackbar').removeClass('show');
+	}, 2500);
+
+}
+
+function BootstrapAlertError(message) {
+	$('#snackbar').addClass('show');
+	$('#snackbar').css('background-color','red');
+
+	$('#snackbar').html(message);
+
+	setTimeout(function () {
+		$('#snackbar').removeClass('show');
+		$('#snackbar').css('background-color','#333');
 	}, 2500);
 
 }
