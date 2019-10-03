d3799d9cf1dde43e7313bc1b5931a98fa0f8d48d
MrJeje_ <jeremy.leriche22@gmail.com>
2019-10-04 01:33:32 +0200

    amélioration sécurité

--- Assets/Js/Custom.js
+++ Assets/Js/Custom.js
@@ -1,14 +1,14 @@
-/**
- * Quelques js esthétiques
- * @param {string} LaPage Affecter la classe active dans la navbar a la page active, en cours
- * @param {*} TypeUser Cacher les onglets qui sont de toutes façon innaccessibles en fonction des droits
- * @param {string} NameUser Permet d'afficher le nom du user connecté en haut de l'appli
- */
-function ClassActive(LaPage, TypeUser, NameUser) {
+// Quelques js esthétiques
+function ClassActive(NameUser) {
   //Enlever actif a toutes les class
   $("exampleAccordion li").each(function () {
     $(this).removeClass("active");
   });
+  
+  if(window.location.href.split('=')[1] == undefined)
+    var LaPage = 'Accueil'
+  else
+    var LaPage = window.location.href.split('=')[1]
 
   //Puis cherche la nav qui a la page d'affichéE et lui donner la class active
   $('nav a[href^="index.php?vue=' + LaPage + '"]').closest("li").addClass("active").closest("ul").addClass("show");
@@ -17,18 +17,12 @@ function ClassActive(LaPage, TypeUser, NameUser) {
   $("#TitreNavBar").text("Ns Scrum - " + NameUser);
 }
 
-/**
- * Active ou désactive le css sidenav-toggled quand clic ou non sur la navbar "gestion"
- */
+// Active ou désactive le css sidenav-toggled quand clic ou non sur la navbar "gestion"
 $("#SlideNav").click(function () {
   $("body").toggleClass("sidenav-toggled");
 });
 
-/**
- * Fonction pour chercher dans une table depuis un input
- * @param {string} IdDivBarreDeRecherche Id de l'input de la barre de recherche sur lequel on va check quand on tape du text
- * @param {string} IdTableRecherche id table sur laquel appliquer la recherche
- */
+// Fonction pour chercher dans une table depuis un input
 function BarreDeRecherche(IdDivBarreDeRecherche, IdTableRecherche) {
   $("#" + IdDivBarreDeRecherche).on("keyup", function () {
     $("#" + IdTableRecherche + " tr:not(thead tr)").filter(function () {
@@ -37,11 +31,7 @@ function BarreDeRecherche(IdDivBarreDeRecherche, IdTableRecherche) {
   });
 }
 
-/**
- * Fonction pour appliquer ce qui se trouve dans la barre de recherche sur la table, sans besoin de taper du texte dedans, c'est instentané
- * @param {string} IdDivBarreDeRecherche Id de l'input de la barre de recherche sur lequel on va check quand on tape du text
- * @param {string} IdTableRecherche id table sur laquel appliquer la recherche
- */
+//  Fonction pour appliquer ce qui se trouve dans la barre de recherche sur la table, sans besoin de taper du texte dedans, c'est instentané
 function GarderLaRecherche(IdDivBarreDeRecherche, IdTableRecherche) {
   $("#" + IdTableRecherche + " tr:not(thead tr)").filter(function () {
     $(this).toggle($(this).text().toLowerCase().indexOf($("#" + IdDivBarreDeRecherche).val().toLowerCase()) > -1);
