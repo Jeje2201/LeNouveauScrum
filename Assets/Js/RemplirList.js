1d82008e6783d285553e7cacaea6ab2129062309
MrJeje_ <jeremy.leriche22@gmail.com>
2020-01-06 16:29:11 +0100

    maj doc

--- Assets/Js/RemplirList.js
+++ Assets/Js/RemplirList.js
@@ -2,9 +2,10 @@ encodeURIComponent
 
 /**
  * This function will fetch data from table and display under <div id="[DivId]">
- * @param {string} DivId Div ou se placera le resultat de la reqete php
+ * @param {string} DivId ID de la div ou se placera le resultat de la reqete php
  * @param {string} action Argument 'action' envoyé en parametre dans le fichier php
  * @param {string} path Path du fichier php
+ * @param {string} id id donné aux templates générés si on les veux dynamiques pour appeler deux fois la meme action et lui donner 2 id différents
  */
 function RequeteAjax(DivId, action, path, id) {
   $.ajax({
