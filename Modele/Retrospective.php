0b645d49c609c70d7af2e77bece42d738fefe2a5
MrJeje_ <jeremy.leriche22@gmail.com>
2020-01-20 03:40:36 +0100

    gestion des remarques json

--- Modele/Retrospective.php
+++ Modele/Retrospective.php
@@ -7,184 +7,138 @@
       if ($_POST["action"] == "LoadObjectif") {
         $numero = $_POST["idAffiche"];
         $statement = $connection->prepare("SELECT O.id,
-      O.objectif,
-      S.couleur,
-      P.nom AS projet,
-      CONCAT(employe.prenom,'&nbsp;', employe.initial) AS Employe,
-      S.nom AS etat
-      FROM objectif O
-      INNER JOIN statutobjectif S
-        ON S.id = O.id_StatutObjectif
-      INNER JOIN projet P
-        ON P.id = O.id_Projet
-      INNER JOIN employe
-        ON employe.id = O.id_Employe
-      WHERE O.id_Sprint = $numero
-      ORDER BY Employe, P.nom, O.id");
-        $statement->execute();
-        $result = $statement->fetchAll();
-        $output = '';
-        $output .= '
-      <table class="table table-sm table-striped table-bordered" id="datatable" >
-      <thead class="thead-light">
-      <tr>
-      <th>N°</th>
-      <th>État</th>
-      <th>Ressource</th>
-      <th>Projet</th>
-      <th>Objectif</th>';
+        O.objectif,
+        S.couleur,
+        P.nom AS projet,
+        CONCAT(employe.prenom,'&nbsp;', employe.initial) AS Employe,
+        S.nom AS etat
+        FROM objectif O
+        INNER JOIN statutobjectif S
+          ON S.id = O.id_StatutObjectif
+        INNER JOIN projet P
+          ON P.id = O.id_Projet
+        INNER JOIN employe
+          ON employe.id = O.id_Employe
+        WHERE O.id_Sprint = $numero
+        ORDER BY Employe, P.nom, O.id");
+          $statement->execute();
+          $result = $statement->fetchAll();
+          $output = '';
+          $output .= '
+        <table class="table table-sm table-striped table-bordered" id="datatable" >
+        <thead class="thead-light">
+        <tr>
+        <th>N°</th>
+        <th>État</th>
+        <th>Ressource</th>
+        <th>Projet</th>
+        <th>Objectif</th>';
+
+      
         if ($_SESSION['Admin'])
           $output .= '<th colspan="2"><center>Changer État</center></th>';
         $output .= '
-      </tr>
-      </thead>
-      <tbody id="myTable">
-      ';
-        if ($statement->rowCount() > 0) {
-          $num = 0;
-          foreach ($result as $row) {
-            $num += 1;
-            $output .= '
-        <tr >
-        
-        <td>' . $num . '</td>
-        <td class="centered font-weight-bold text-white" style="background-color: ' . $row["couleur"] . ';">' . $row["etat"] . '</td>
-        <td>' . $row["Employe"] . '</td>
-        <td>' . $row["projet"] . '</td>
-        <td>' . $row["objectif"] . '</td>';
-            if ($_SESSION['Admin'])
+        </tr>
+        </thead>
+        <tbody id="myTable">
+        ';
+          if ($statement->rowCount() > 0) {
+            $num = 0;
+            foreach ($result as $row) {
+              $num += 1;
               $output .= '
-        <td><center><div class="btn-group" role="group" >
-        <button type="button" id="' . $row["id"] . '" class="btn btn-success 1"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i></button>
-        <button type="button" id="' . $row["id"] . '" class="btn btn-warning 2"><i class="fa fa-hourglass-half" aria-hidden="true"></i></button>
-        <button type="button" id="' . $row["id"] . '" class="btn btn-danger 3"><i class="fa fa-thumbs-o-down" aria-hidden="true"></i></button>
-        <button type="button" id="' . $row["id"] . '" class="btn btn-dark 4"><i class="fa fa-times" aria-hidden="true"></i></button>
-        <button type="button" id="' . $row["id"] . '" class="btn btn-primary 5"><i class="fa fa-question" aria-hidden="true"></i></button>
-        </div></center></td>
-        
-        <td>
-        <center>
-        <button type="button" id="' . $row["id"] . '" class="btn btn-warning GetObjectif"><i class="fa fa-pencil" aria-hidden="true"></i></button>
-        </center>
-        </td>
+          <tr >
+          
+          <td>' . $num . '</td>
+          <td class="centered font-weight-bold text-white" style="background-color: ' . $row["couleur"] . ';">' . $row["etat"] . '</td>
+          <td>' . $row["Employe"] . '</td>
+          <td>' . $row["projet"] . '</td>
+          <td>' . $row["objectif"] . '</td>';
+              if ($_SESSION['Admin'])
+                $output .= '
+          <td><center><div class="btn-group" role="group" >
+          <button type="button" id="' . $row["id"] . '" class="btn btn-success 1"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i></button>
+          <button type="button" id="' . $row["id"] . '" class="btn btn-warning 2"><i class="fa fa-hourglass-half" aria-hidden="true"></i></button>
+          <button type="button" id="' . $row["id"] . '" class="btn btn-danger 3"><i class="fa fa-thumbs-o-down" aria-hidden="true"></i></button>
+          <button type="button" id="' . $row["id"] . '" class="btn btn-dark 4"><i class="fa fa-times" aria-hidden="true"></i></button>
+          <button type="button" id="' . $row["id"] . '" class="btn btn-primary 5"><i class="fa fa-question" aria-hidden="true"></i></button>
+          </div></center></td>
+          
+          <td>
+          <center>
+          <button type="button" id="' . $row["id"] . '" class="btn btn-warning GetObjectif"><i class="fa fa-pencil" aria-hidden="true"></i></button>
+          </center>
+          </td>
+          </tr>
+          ';
+          }
+        } else {
+          $output .= '
+        <tr>
+        <td align="center" colspan="10">Pas de données</td>
         </tr>
         ';
+            }
+            $output .= '</tbody></table>';
+            print $output;
           }
-        } else {
-          $output .= '
-     <tr>
-     <td align="center" colspan="10">Pas de données</td>
-     </tr>
-     ';
-        }
-        $output .= '</tbody></table>';
-        print $output;
-      }
 
-      if ($_POST["action"] == "LoadMail") {
+      
+          if ($_POST["action"] == "LoadMail") {
         $numero = $_POST["idAffiche"];
         $statement = $connection->prepare("SELECT O.id,
-      O.objectif,
-      S.couleur,
-      projet.nom AS projet,
-      S.nom AS etat
-      FROM objectif O
-      INNER JOIN statutobjectif S
-        ON S.id = O.id_StatutObjectif
-      INNER JOIN projet
-        ON projet.id = O.id_Projet
-      WHERE O.id_Sprint = $numero
-      ORDER BY projet.nom");
-        $statement->execute();
-        $result = $statement->fetchAll();
-        $output = 'Rétrospective:';
+        O.objectif,
+        S.couleur,
+        projet.nom AS projet,
+        S.nom AS etat
+        FROM objectif O
+        INNER JOIN statutobjectif S
+          ON S.id = O.id_StatutObjectif
+        INNER JOIN projet
+          ON projet.id = O.id_Projet
+        WHERE O.id_Sprint = $numero
+        ORDER BY projet.nom");
+          $statement->execute();
+          $result = $statement->fetchAll();
+          $output = 'Rétrospective:';
 
-        $projet = '';
+          $projet = '';
 
-        if ($statement->rowCount() > 0) {
-          foreach ($result as $row) {
+          if ($statement->rowCount() > 0) {
+            foreach ($result as $row) {
 
-            if ($projet != $row["projet"]) {
-              $projet = $row["projet"];
-              $output .= '<br> <br><u>' . $row["projet"] . '</u>: <br>' . '- ' . $row["objectif"] . ' (<span class="Mail' . $row["etat"] . '">' . $row["etat"] . '</span>)';
-            } else {
-              $output .= '<br>- ' . $row["objectif"] . ' (<span class="Mail' . $row["etat"] . '">' . $row["etat"] . '</span>)';
+              if ($projet != $row["projet"]) {
+                $projet = $row["projet"];
+                $output .= '<br> <br><u>' . $row["projet"] . '</u>: <br>' . '- ' . $row["objectif"] . ' (<span class="Mail' . $row["etat"] . '">' . $row["etat"] . '</span>)';
+              } else {
+                $output .= '<br>- ' . $row["objectif"] . ' (<span class="Mail' . $row["etat"] . '">' . $row["etat"] . '</span>)';
+              }
             }
           }
-        }
 
-        $output .= '<br> <br> Remarques: <br>';
+          $output .= '<br> <br> Remarques: <br>';
 
-        $statement = $connection->prepare("SELECT label
-      FROM retrospective R
-      WHERE R.DateFini IS NULL");
-        $statement->execute();
-        $result = $statement->fetchAll();
+          $statement = $connection->prepare("SELECT label
+        FROM retrospective R
+        WHERE R.DateFini IS NULL");
+          $statement->execute();
+          $result = $statement->fetchAll();
 
-        if ($statement->rowCount() > 0) {
-          foreach ($result as $row) {
-            $output .= '- ' . $row["label"] . '<br>';
+          if ($statement->rowCount() > 0) {
+            foreach ($result as $row) {
+              $output .= '- ' . $row["label"] . '<br>';
+            }
           }
+
+          print ($output);
         }
 
-        print ($output);
-      }
-
-      if ($_POST["action"] == "LoadRemarque") {
-        $statement = $connection->prepare("SELECT id,
-      DateCreation,
-      Label
-      FROM retrospective
-      WHERE DateFini  IS NULL
-      ORDER BY DateCreation DESC");
-        $statement->execute();
-        $result = $statement->fetchAll();
-        $output = '';
-        $output .= '
-      <table class="table table-sm table-striped table-bordered" id="datatable" width="100%" cellspacing="0">
-      <thead class="thead-light">
-      <tr>
-      <th>Création</th>
-      <th>Remarque</th>';
-        if ($_SESSION['Admin'])
-          $output .= '<th width=""><center>Changer État</center></th>';
-
-        $output .= ' </tr>
-      </thead>
-      <tbody id="myTable">
-      ';
-        if ($statement->rowCount() > 0) {
-          foreach ($result as $row) {
-            $output .= '
-        <tr>
-        <td>' . date("d/m/Y", strtotime($row["DateCreation"])) . '</td>
-        <td>' . $row["Label"] . '</td>';
-        if ($_SESSION['Admin']){
-          $output .= '
-          <td><center><div class="btn-group" role="group" >
-          <button type="button" id="' . $row["id"] . '" class="btn btn-warning GetRemarque"><i class="fa fa-pencil" aria-hidden="true"></i></button>
-          <button type="button" id="' . $row["id"] . '" class="btn btn-success ValiderRemarque"><i class="fa fa-check" aria-hidden="true"></i></button>
-          </div></center></td>';
-        }
-          $output .= '</tr>
-        ';
-          }
-        } else {
-          $output .= '
-     <tr>
-     <td align="center" colspan="10">Pas de données</td>
-     </tr>
-     ';
-        }
-        $output .= '</tbody></table>';
-        print $output;
-      }
-
-      if ($_POST["action"] == "Créer") {
+      
+        if ($_POST["action"] == "Créer") {
         $statement = $connection->prepare("
-   INSERT INTO objectif (id_Sprint, id_Projet, id_Employe, objectif, id_StatutObjectif) 
-   VALUES (:id_Sprint, :id_Projet, :id_Employe, :objectif, :id_StatutObjectif)
-   ");
+        INSERT INTO objectif (id_Sprint, id_Projet, id_Employe, objectif, id_StatutObjectif) 
+        VALUES (:id_Sprint, :id_Projet, :id_Employe, :objectif, :id_StatutObjectif)
+        ");
         $result = $statement->execute(
           array(
             ':id_Sprint' => $_POST["idSprint"],
@@ -196,22 +150,6 @@
         );
         if (!empty($result))
           print 'Objectif "'.$_POST["LabelObjectif"].'" créé';
-        else
-          print_r($statement->errorInfo());
-      }
-
-      if ($_POST["action"] == "Créer Retrospective") {
-        $statement = $connection->prepare("
-   INSERT INTO retrospective (Label, DateCreation) 
-   VALUES (:Label, NOW())
-   ");
-        $result = $statement->execute(
-          array(
-            ':Label' => $_POST["Labelretrospective"]
-          )
-        );
-        if (!empty($result))
-          print 'Remarque "'.$_POST["Labelretrospective"].'" créée';
         else
           print_r($statement->errorInfo());
       }
@@ -234,27 +172,12 @@
         print json_encode($output);
       }
 
-      if ($_POST["action"] == "SelectRemarque") {
-        $output = array();
-        $statement = $connection->prepare(
-          "SELECT * FROM retrospective 
-        WHERE id = '" . $_POST["id"] . "' 
-        LIMIT 1"
-        );
-        $statement->execute();
-        $result = $statement->fetch();
-
-        $output["labelremarque"] = $result["Label"];
-
-        print json_encode($output);
-      }
-
       if ($_POST["action"] == "Changer Objectif") {
         $statement = $connection->prepare(
           "UPDATE objectif 
-   SET objectif = :LabelObjectif, id_Sprint = :id_Sprint, id_Projet = :id_Projet, id_Employe = :id_Employe
-   WHERE id = :id
-   "
+        SET objectif = :LabelObjectif, id_Sprint = :id_Sprint, id_Projet = :id_Projet, id_Employe = :id_Employe
+        WHERE id = :id
+        "
         );
         $result = $statement->execute(
           array(
@@ -271,31 +194,12 @@
           print_r($statement->errorInfo());
       }
 
-      if ($_POST["action"] == "Changer Remarque") {
-        $statement = $connection->prepare(
-          "UPDATE retrospective
-   SET Label = :LabelRemarque
-   WHERE id = :id
-   "
-        );
-        $result = $statement->execute(
-          array(
-            ':LabelRemarque' => $_POST["Labelretrospective"],
-            ':id' => $_POST["id"]
-          )
-        );
-        if (!empty($result))
-          print 'Remarque "'.$_POST["Labelretrospective"].'" changée';
-        else
-          print_r($statement->errorInfo());
-      }
-
       if ($_POST["action"] == "ChangerEtat") {
         $statement = $connection->prepare(
           "UPDATE objectif 
-   SET id_StatutObjectif = :EtatObjectif 
-   WHERE id = :id
-   "
+          SET id_StatutObjectif = :EtatObjectif 
+          WHERE id = :id
+          "
         );
         $result = $statement->execute(
           array(
@@ -305,24 +209,6 @@
         );
         if (!empty($result))
           print 'Etat changé';
-        else
-          print_r($statement->errorInfo());
-      }
-
-      if ($_POST["action"] == "retrospectiveFini") {
-        $statement = $connection->prepare(
-          "UPDATE retrospective 
-   SET DateFini = NOW()
-   WHERE id = :id
-   "
-        );
-        $result = $statement->execute(
-          array(
-            ':id' => $_POST["id"]
-          )
-        );
-        if (!empty($result))
-          print 'Remarque validée';
         else
           print_r($statement->errorInfo());
       }
---
+++ Modele/Retrospective_Remarque.php
@@ -0,0 +1,142 @@
+<?php
+session_start();
+require_once('../Modele/Configs.php');
+
+if (isset($_POST["action"])) {
+
+  if ($_POST["action"] == "getRemarques") {
+    $statement = $connection->prepare("SELECT
+        R.id as remarque_id,
+        R.Label as remarque_label,
+        R.DateCreation as remarque_dateCreation,
+        R.DateFini as remarque_dateFini
+        FROM retrospective R
+        ORDER BY remarque_dateFini ASC");
+
+    $statement->execute();
+    $result = $statement->fetchAll();
+
+    $resultat = [];
+
+    foreach ($result as $row) {
+
+      $MonTest = [];
+
+      $MonTest['remarque_id'] = $row['remarque_id'];
+      $MonTest['remarque_label'] = $row['remarque_label'];
+
+      if ($row["remarque_dateCreation"] == null)
+        $MonTest["remarque_dateCreation"] = "";
+      else
+        $MonTest["remarque_dateCreation"] = date("d/m/Y", strtotime($row["remarque_dateCreation"]));
+
+      if ($row["remarque_dateFini"] == null)
+        $MonTest["remarque_dateFini"] = "";
+      else
+        $MonTest["remarque_dateFini"] = date("d/m/Y", strtotime($row["remarque_dateFini"]));
+
+      $resultat[] = $MonTest;
+    }
+
+    print json_encode($resultat);
+  }
+
+  if ($_POST["action"] == "getRemarque") {
+
+    $statement = $connection->prepare(
+      "SELECT * FROM retrospective 
+     WHERE id = '" . $_POST["remarque_id"] . "' 
+     LIMIT 1"
+    );
+    $statement->execute();
+    $result = $statement->fetch();
+
+    $resultat = [];
+
+    $resultat["RetrospectiveRemarque_DateCreation"] = date("d-m-Y", strtotime($result["DateCreation"]));
+
+    if ($result["DateFini"] == null)
+      $resultat["RetrospectiveRemarque_DateFini"] = "";
+    else
+      $resultat["RetrospectiveRemarque_DateFini"] = date("d-m-Y", strtotime($result["DateFini"]));
+
+    $resultat["RetrospectiveRemarque_Label"] = $result["Label"];
+    $resultat["RetrospectiveRemarque_id"] = $result["id"];
+
+    print json_encode($resultat);
+  }
+
+  if ($_POST["action"] == "achieveRetrospective") {
+    $statement = $connection->prepare(
+      "UPDATE retrospective 
+      SET DateFini = NOW()
+      WHERE id = :id
+      "
+    );
+    $result = $statement->execute(
+      array(
+        ':id' => $_POST["remarque_id"]
+      )
+    );
+    if (!empty($result))
+      print true;
+    else
+      print_r($statement->errorInfo());
+  }
+
+  if ($_POST["action"] == "addRemarque") {
+    $statement = $connection->prepare("
+   INSERT INTO retrospective (Label, DateCreation) 
+   VALUES (:Label, NOW())
+   ");
+    $result = $statement->execute(
+      array(
+        ':Label' => $_POST["Labelretrospective"]
+      )
+    );
+    if (!empty($result))
+      print true;
+    else
+      print_r($statement->errorInfo());
+  }
+
+  if ($_POST["action"] == "putRemarque") {
+
+    if ($_POST["remarque_DateFin"] == "undefined-undefined-")
+      $_POST["remarque_DateFin"] = null;
+      
+    $statement = $connection->prepare(
+      "UPDATE retrospective
+      SET Label = :remarque_label, DateCreation = :remarque_DateDebut, DateFini = :remarque_DateFin
+      WHERE id = :remarque_id"
+    );
+    $result = $statement->execute(
+      array(
+        ':remarque_label' => $_POST["remarque_label"],
+        ':remarque_DateDebut' => $_POST["remarque_DateDebut"],
+        ':remarque_DateFin' => $_POST["remarque_DateFin"],
+        ':remarque_id' => $_POST["remarque_id"]
+      )
+    );
+    if (!empty($result))
+      print true;
+    else
+      print_r($statement->errorInfo());
+  }
+
+  if ($_POST["action"] == "dellRemarque") {
+    $statement = $connection->prepare(
+      "DELETE FROM retrospective
+         WHERE id = :id"
+    );
+    $result = $statement->execute(
+      array(
+        ':id' => $_POST["remarque_id"]
+      )
+    );
+    if ($statement->rowCount() > 0)
+      print true;
+    else
+      print_r($statement->errorInfo());
+  }
+}
