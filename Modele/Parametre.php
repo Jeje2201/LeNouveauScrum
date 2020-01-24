cbc570f5353ec8eec3fd3a1a781b0b6c7e7037d2
MrJeje_ <jeremy.leriche22@gmail.com>
2020-01-24 13:21:30 +0100

    Pas besoin de liste total, peut l'avoir avec les inputs dates

--- Modele/Parametre.php
+++ Modele/Parametre.php
@@ -73,57 +73,6 @@
         print $output;
       }
 
-      //List des heures par sprint de rempli
-      if ($_POST["action"] == "LoadFicheDeTempsValide") {
-        $output = array();
-        $statement = $connection->prepare(
-        "SELECT sum(cir.Time) as 'Temps', projet.nom as 'Projets' from cir inner join projet on cir.Fk_Project = projet.id where Fk_User = " . $idRessource . " group by Fk_Project order by Temps desc");
-        
-        $statement->execute();
-        $result = $statement->fetchAll();
-        $output = '';
-        $output .= '
-        <table class="table table-sm table-striped table-bordered" id="datatable" width="100%" cellspacing="0">
-        <thead class="thead-light">
-        <tr>
-        <th width="">Projet</th>
-        <th width="">Total</th>
-        </tr>
-        </thead>
-        <tbody id="myTable">
-        ';
-        if ($statement->rowCount() > 0) {
-          foreach ($result as $row) {
-
-            $num = $row["Temps"];
-            $hours = ($num / 60);
-            $rhours = floor($hours);
-            $minutes = ($hours - $rhours) * 60;
-            $rminutes = round($minutes);
-            if($rminutes < 10){
-              $rminutes = "0".$rminutes;
-            }
-
-            if($rhours < 10){
-              $rhours = "0".$rhours;
-            }
-
-            $output .= '
-            <tr>
-            <td>' . $row["Projets"] . '</td>
-            <td>' . $rhours . 'h' . $rminutes . '</td>
-            </tr>';
-          }
-        } else {
-          $output .= '
-          <tr>
-            <td align="center" colspan="10">Pas de données</td>
-          </tr>';
-        }
-        $output .= '</tbody></table>';
-        print $output;
-      }
-
       if ($_POST["action"] == "CustomColorName") {
         $statement = $connection->prepare(
           "UPDATE employe 
