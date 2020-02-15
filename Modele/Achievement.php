b8f2e470d7e94bb52944615afc8822831986092c
MrJeje_ <jeremy.leriche22@gmail.com>
2020-02-15 17:04:04 +0100

    achievement fonctionnel

--- Modele/Achievement.php
+++ Modele/Achievement.php
@@ -8,6 +8,16 @@
       //Load les fiches de temps de tout le monde dans la gestion des fiches de temps
       if ($_POST["action"] == "Trophee") {
 
+        $output = array();
+        $statement = $connection->prepare(
+          "SELECT sum(fiche_de_temps_time)/60/7.4 from fiche_de_temps
+          where fiche_de_temps_fk_user = 22
+          and fiche_de_temps_fk_projet = (SELECT projet_pk from projet where projet_nom like 'Congés')"
+        );
+        $statement->execute();
+        $output['TotJourDeConge'] =  $statement->fetch(PDO::FETCH_COLUMN);
+
+        print json_encode($output);
       }
 
     }
