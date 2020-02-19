94bc36412f4ec5049cc7ce2398db4e92bc0af1bb
MrJeje_ <jeremy.leriche22@gmail.com>
2020-02-19 13:04:14 +0100

    gestion tache dans tache

--- Modele/Tache.php
+++ Modele/Tache.php
@@ -4,6 +4,82 @@
 
     if (isset($_POST["action"])) {
 
+
+      if ($_POST["action"] == "DeleteTache") {
+        $statement = $connection->prepare(
+          "DELETE FROM tache
+         WHERE tache_pk = :id"
+        );
+        $result = $statement->execute(
+          array(
+            ':id' => $_POST["id"]
+          )
+        );
+        if ($statement->rowCount() > 0)
+          print true;
+        else
+          print_r($statement->errorInfo());
+      }
+
+            //Mettre a jour une tache
+            if ($_POST["action"] == "UpdateTache") {
+
+              if ($_POST["Done"] == "")
+                $_POST["Done"] = null;
+      
+              $statement = $connection->prepare(
+                "UPDATE tache
+                SET
+                tache_heure = :heure,
+                tache_fk_sprint = :id_Sprint,
+                tache_fk_projet = :id_Projet,
+                tache_done = :Done,
+                tache_label = :Label,
+                tache_fk_user = :id_Employe 
+                WHERE tache_pk = :id"
+              );
+              $result = $statement->execute(
+                array(
+                  ':heure' => $_POST["NombreHeure"],
+                  ':Done' => $_POST["Done"],
+                  ':Label' => $_POST["Label"],
+                  ':id_Sprint' => $_POST["idSprint"],
+                  ':id_Projet' => $_POST["idProjet"],
+                  ':id_Employe' => $_POST["idEmploye"],
+                  ':id' => $_POST["id"]
+                )
+              );
+              if (!empty($result))
+                print true;
+              else
+                print_r($statement->errorInfo());
+            }
+
+      //Get les information d'une tache
+      if ($_POST["action"] == "GetTache") {
+        $output = array();
+        $statement = $connection->prepare(
+          "SELECT * FROM tache 
+         WHERE tache_pk = '" . $_POST["id"] . "' 
+         LIMIT 1"
+        );
+        $statement->execute();
+        $result = $statement->fetch();
+
+        $output["heure"] = $result["tache_heure"];
+
+        if ($result["tache_done"] == null)
+          $output["Done"] = "";
+        else
+          $output["Done"] = date("d-m-Y", strtotime($result["tache_done"]));
+
+        $output["Label"] = $result["tache_label"];
+        $output["id_Employe"] = $result["tache_fk_user"];
+        $output["id_Projet"] = $result["tache_fk_projet"];
+
+        print json_encode($output);
+      }
+
       //Fonction pour afficher les cards a DESCendre et DESCendues
       if ($_POST["action"] == "AfficherCards") {
 
