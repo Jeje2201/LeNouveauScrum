a348fe8afd3320463e620ec70329d698800cba84
MrJeje_ <jeremy.leriche22@gmail.com>
2020-02-23 17:11:41 +0100

    parametre json encode

--- Modele/Parametre.php
+++ Modele/Parametre.php
@@ -4,7 +4,7 @@
 
     if (isset($_POST["action"])) {
 
-      $idRessource = $_SESSION['user']['id'];
+      // $idRessource = $_SESSION['user']['id'];
 
       if ($_POST["action"] == "LoadTacheValide") {
         $output = array();
@@ -13,41 +13,14 @@
         projet_nom
         FROM `tache`
         INNER JOIN projet on tache_fk_projet = projet_pk
-        where tache_fk_user = " . $idRessource . "
+        where tache_fk_user = " . $_SESSION['user']['id'] . "
         and tache_done is not null
         group by tache_fk_projet
         order by Temps desc");
         
         $statement->execute();
-        $result = $statement->fetchAll();
-        $output = '';
-        $output .= '
-        <table class="table table-sm table-striped table-bordered" id="datatable" width="100%" cellspacing="0">
-        <thead class="thead-light">
-        <tr>
-        <th width="">Projet</th>
-        <th width="">Total</th>
-        </tr>
-        </thead>
-        <tbody id="myTable">
-        ';
-        if ($statement->rowCount() > 0) {
-          foreach ($result as $row) {
-
-            $output .= '
-            <tr>
-            <td>' . $row["projet_nom"] . '</td>
-            <td>' . $row["Temps"] . 'h</td>
-            </tr>';
-          }
-        } else {
-          $output .= '
-          <tr>
-            <td align="center" colspan="10">Pas de données</td>
-          </tr>';
-        }
-        $output .= '</tbody></table>';
-        print $output;
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+        print json_encode($result);
       }
     }
 
